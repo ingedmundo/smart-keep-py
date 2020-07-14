ef94837ad322fcaecd2c2f86a61d5fd3a7716523
Edmundo Perez <edmundo@michelada.io>
2020-07-14 18:59:26 -0500

    List and Item models added

---
+++ todos/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class List(models.Model):
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return  self.description
+
+class Item(models.Model):
+    description = models.CharField(max_length=100)
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return  f"{self.description} on {self.list}"

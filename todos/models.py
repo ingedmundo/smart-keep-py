f73a90ea93a8104ed22c628de6f4ea7e0adcb963
Edmundo Perez <edmundo@michelada.io>
2020-07-22 18:52:38 -0500

    list budget

--- todos/models.py
+++ todos/models.py
@@ -5,10 +5,19 @@ class List(models.Model):
 
     def __str__(self):
         return  self.description
+    
+    def budget(self):
+        budget = 0
+        for item in self.item_set.filter(done=False):
+            budget += item.cost 
+
+        return budget
+
 
 class Item(models.Model):
     description = models.CharField(max_length=100)
     done = models.BooleanField(default=False)
+    cost = models.DecimalField( max_digits=5, decimal_places=2, default=1.0)
     list = models.ForeignKey(List, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -16,3 +25,5 @@ class Item(models.Model):
     
     def toggleDone(self):
         self.done = not self.done
+
+    

399961959ecda5b03be6c541ed5b111a28ef245c
Edmundo Perez <edmundo@michelada.io>
2020-08-09 22:07:00 -0500

    WIP react frontend app

--- smartkeeppy/urls.py
+++ smartkeeppy/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('website/', include('website.urls')),
+    path('', include('todos.urls')),
     path('todos/', include('todos.urls')),
-    path('', include('todos.urls'))
+    path('frontend', include('frontend.urls'))
 ]
